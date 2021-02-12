773cd1e5285dfd369805d060264434d3ac6c68d4
Antoine Carnec <antoinecarnec@gmail.com>
2021-02-12 17:04:07 +0000

    Put argument handling into it's own file.

--- bfetch/modules/arguments.py
+++ bfetch/modules/arguments.py
@@ -7,15 +7,28 @@ def arguments() -> Tuple[bool, float]:
     fast = 1.5
     slow = 0.1
     default = 0.8
-    parser.add_argument("-s", "--show", default=False, action="store_true",
-                        help="Whether to show the browser.")
     parser.add_argument(
-        "-S", "--slow", action="store_const", default=None, const=slow,
-        help="Dowloads files at a slower sleep"
+        "-s",
+        "--show",
+        default=False,
+        action="store_true",
+        help="Whether to show the browser.",
     )
     parser.add_argument(
-        "-F", "--fast", action="store_const", default=None, const=fast,
-        help="Downloads the files as fast as possible."
+        "-S",
+        "--slow",
+        action="store_const",
+        default=None,
+        const=slow,
+        help="Dowloads files at a slower sleep",
+    )
+    parser.add_argument(
+        "-F",
+        "--fast",
+        action="store_const",
+        default=None,
+        const=fast,
+        help="Downloads the files as fast as possible.",
     )
     args = parser.parse_args()
 
@@ -25,5 +38,5 @@ def arguments() -> Tuple[bool, float]:
         speed = args.fast
     else:
         speed = default
-    
+
     return args.show, speed
